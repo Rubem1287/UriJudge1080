429fedd79a6c4631142c97cea282601dd125fea5
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-16 16:04:46 -0300

    Add files via upload

---
+++ Uri1080/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Uri1080
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int v,p;
+            int[] vetor;
+            v = 0;
+            p = 0;
+            vetor = new int[100];
+            v = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 100; i++)
+            {
+                vetor[i] = int.Parse(Console.ReadLine());
+                int a = vetor[i];
+                if(a >v)
+                {
+                    v = a;
+                    p = i;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(v);
+            Console.WriteLine();
+            Console.WriteLine(p);
+        }
+    }
+}
